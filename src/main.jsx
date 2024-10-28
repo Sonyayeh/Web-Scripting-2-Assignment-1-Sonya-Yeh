67d4a6d66c98a5de7f63b5124b07d9b7bf792c1b
Sonya <cyeh27@my.bcit.ca>
2024-10-28 12:48:21 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,9 @@ import App from './App.jsx'
 import Items from './data/Items.json'
 import './index.css'
 
+// createRootcreates a root for the react application
+// then, it targets an HTML element with the ID root
+// the app component will be rendered inside the strictMode 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
